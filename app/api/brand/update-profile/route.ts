6b1cf800781d977578a628bc63a22602ed4d7c55
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2025-12-17 12:48:27 +0100

    fix

--- app/api/brand/update-profile/route.ts
+++ app/api/brand/update-profile/route.ts
@@ -51,7 +51,7 @@ export async function PATCH(request: Request) {
       if (error) {
         return NextResponse.json(
           { error: error.message },
-          { status: error.status || 500 },
+          { status: 500 },
         );
       }
     }
